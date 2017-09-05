94fa148a8a023f0b1c3133d8ef503c990d289864
RichardLin3 <rslin93@gmail.com>
2017-09-05 12:01:57 -0400

    Inserts into duedate and description table
    does not insert into tasks table for some reason

--- crud.php
+++ crud.php
@@ -1,37 +1,32 @@
 <?php
 	session_start();
-	
+	$descriptionID = "";
+	$dueDateID = "";	
 	$taskID = "";
 	$taskTitle = "";
-	$taskDescription = "";
-
-	$edit_state = false;
+	$taskDetails = "";
+	$dueDate = "";
 
 	include("dbCon.php");
 
 	//if save button is clicked
 	if (isset($_POST['save'])){
 		$taskTitle = $_POST['taskTitle'];
-		$taskDescription = $_POST['taskDescription'];
+		$taskDetails = $_POST['taskDetails'];
+		$dueDate = $_POST['dueDate'];
 
-		$query = "INSERT INTO tasks (title, description) VALUES ('$taskTitle', '$taskDescription')";
-		mysqli_query($conn, $query);
+		$query1 = "INSERT INTO description (details) VALUES ('$taskDetails')";
+		mysqli_query($conn,$query1);
+
+		$query2 = "INSERT INTO duedate (setDate) VALUES ('$dueDate')";
+		mysqli_query($conn,$query2);
+
+		$query = "INSERT INTO tasks (title, description_fk, duedate_fk) VALUES ('$taskTitle', '$descriptionID', '$dueDateID')";
+		mysqli_query($conn,$query);
 
 		$_SESSION['msg'] = "Entry Saved";
 		header('location: crudView.php');
 	}
-
-	//update Records
-	if (isset($_POST['edit'])) {
-		$taskTitle = ($_POST['taskTitle']);
-		$taskDescription = ($_POST['taskDescription']);
-		
-		mysqli_query($conn, "UPDATE tasks SET title='$taskTitle', description='$taskDescription' WHERE taskID='$taskID'");
-		
-		$_SESSION['msg'] = "Entry Updated";
-		header('location: crudView.php');
-	}
-
 
 	//Delete Records
 	if( isset($_GET['delete'])) {
@@ -39,11 +34,20 @@
 		mysqli_query($conn, "DELETE FROM tasks WHERE taskID = '$taskID'");
 		
 		$_SESSION['msg'] = "Entry Deleted";
-		header('location: CRUDview.php');
+		header('location: crudView.php');
 	}
 
 
-
 	//Retrieve Records
-	$results = mysqli_query($conn, "SELECT t.taskID as taskID, t.title as taskTitle, t.description as taskDescription FROM tasks as t");
+	$results = mysqli_query($conn, "
+		SELECT 
+			t.taskID as taskID,
+			t.title as taskTitle, 
+			d.descriptionID as descriptionID,
+			d.details as taskDetails,
+			du.dueDateID as dueDateID,
+			du.setDate as dueDate
+		FROM tasks as t
+		INNER JOIN description as d ON d.descriptionID=t.description_fk
+		INNER JOIN duedate as du on du.dueDateID=t.duedate_fk");
 ?>
--- crudView.php
+++ crudView.php
@@ -1,25 +1,14 @@
+<!DOCTYPE html>
 <html>
 <head><link rel="stylesheet" href="style.css" type="text/css"></head>
 <div class ="box">
 	<a href ="index.html">Home</a>
 </div>
-<?php include('crud.php');
 
-if(isset($_GET['edit'])) {
-	$taskID = $_GET['edit'];
-	$edit_state = true;
-	$rec = mysqli_query($conn, "SELECT t.taskID as taskID, t.title as taskTitle, t.description as taskDescription FROM tasks as t WHERE taskID = '$taskID'");
-	$record = mysqli_fetch_array($rec);
-
-	$taskID = $record['taskID'];
-	$taskTitle = $record['taskTitle'];
-	$taskDescription = $record['taskDescription'];
-}
-
+<?php 
+	include('crud.php');
 ?>
-<!DOCTYPE html>
-<html>
-<head><link rel="stylesheet" href="style.css" type="text/css"></head>
+
 <body>
 	<?php if(isset($_SESSION['msg'])): ?>
 		<?php
@@ -32,6 +21,7 @@ if(isset($_GET['edit'])) {
 			<tr>
 				<th>Task Title</th>
 				<th>Description</th>
+				<th>Due Date</th>
 				<th colspan="2">Action</th>
 			</tr>
 		</thead>
@@ -39,10 +29,8 @@ if(isset($_GET['edit'])) {
 			<?php while($row = mysqli_fetch_array($results)) {?>
 			<tr>
 				<td><?php echo $row['taskTitle'] ?></td>
-				<td><?php echo $row['taskDescription'] ?></td>
-				<td>
-					<a href="crudView.php?edit=<?php echo $row['taskID']; ?>">Edit</a>
-				</td>
+				<td><?php echo $row['taskDetails'] ?></td>
+				<td><?php echo $row['dueDate'] ?></td>
 				<td> 
 					<a href="crudView.php?delete=<?php echo $row['taskID']; ?>">Delete</a>
 				</td>	
@@ -51,7 +39,7 @@ if(isset($_GET['edit'])) {
 			<?php } ?>
 		</table>
 		<form method="post" action="crud.php">
-			<input type="hidden" name="taskId" value ="<?php echo $taskID ?>">
+			<input type="hidden" name="taskID" value ="<?php echo $taskID ?>">
 			<br>
 			<div class ="box">
 				<label>Task Title</label>
@@ -60,13 +48,13 @@ if(isset($_GET['edit'])) {
 			<div class ="box">
 				<label>Description</label>
 			</div>
-			<input type="text" name="taskDescription" placeholder=" A short description of what neesd to be done." value ="<?php echo $taskDescription ?>">
+			<input type="text" name="taskDetails" placeholder=" A short description of what neesd to be done." value ="<?php echo $taskDetails ?>">
+			<div class ="box">
+				<label>Due Date</label>
+			</div>
+			<input type="text" name="dueDate" placeholder="MM/DD/YYYY" value ="<?php echo $dueDate ?>">
 
-			<?php if($edit_state == false): ?>
-				<button type="submit" name="save">Save</button>
-			<?php else: ?>
-				<button type="submit" name="edit">Edit</button>
-			<?php endif ?>
+			<button type="submit" name="save">Save</button>
 
 		</form>
 	</body>
